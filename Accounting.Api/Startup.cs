584ab5eb8c97180bfdb2e1486d8c796aad28b3b2
Ivan Batinić <ivek81cro@gmail.com>
2021-03-12 00:16:29 +0100

    Implement JWT

--- Accounting.Api/Startup.cs
+++ Accounting.Api/Startup.cs
@@ -1,20 +1,16 @@
 using Accounting.Api.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Swashbuckle.Swagger;
 using Microsoft.OpenApi.Models;
 using Accounting.DataManager.DataAccess;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System;
 
 namespace Accounting.Api
 {
@@ -38,6 +34,24 @@ namespace Accounting.Api
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", jwtBearerOptions =>
+                {
+                    jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MojTajniKljuč")),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.FromMinutes(5)
+                    };
+                }
+            );
 
             services.AddSwaggerGen(c =>
             {

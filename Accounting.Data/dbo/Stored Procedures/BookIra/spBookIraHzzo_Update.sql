d7bf01d272a3a5d643e8f40b13ef986dd3940320
Ivan Batinić <ivek81cro@gmail.com>
2021-07-13 20:15:00 +0200

    Sort stored procedures in folders

---
+++ Accounting.Data/dbo/Stored Procedures/BookIra/spBookIraHzzo_Update.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spBookIraHzzo_Update]
+@Id INT,
+@Racun NVARCHAR(5),
+@Godina NVARCHAR(4),
+@Iznos DECIMAL(9,2)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE BookIra SET UkupnoUplaceno+=@Iznos, PreostaloZaUplatit-=@Iznos WHERE BrojRacuna=@Racun AND year(Datum)=@Godina;
+	UPDATE BookIraHzzo SET Povezan=1 WHERE Id=@Id;
+
+END
